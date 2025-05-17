edab5f75eb83936cdb072990f37753778c53f17b
EliousMondal <mdeliousali@gmail.com>
2025-05-17 17:02:56 -0400

    added comments to the model file frenkel_biexciton.py

--- MFE/frenkel_biexciton.py
+++ MFE/frenkel_biexciton.py
@@ -6,6 +6,17 @@ import param_Frenkel as param
 
 @nb.jit(nopython=True)
 def H_sb(R):
+    """
+    Description:
+        Computes the contribution from bath to the system.
+    Input     : 
+        R  → current position coordinates of the bath
+    Output    :
+        The site energy change due to R
+    Procedure :
+        Compute δε(t) for each site → δεᵢ(R) = -∑ᵤgᵤ⁽ⁱ⁾Rᵤ⁽ⁱ⁾
+    """
+    
     ε_sb    = np.zeros(param.NStates)
     ε_sb[0] = -np.sum(param.g_nν * R[:param.Modes])
     ε_sb[1] = -np.sum(param.g_nν * R[param.Modes:])
@@ -14,6 +25,17 @@ def H_sb(R):
 
 @nb.jit(nopython=True)
 def H_sys(R):
+    """
+    Description:
+        Generates the system Hamitonian for a R.
+    Input     : 
+        R  → current position coordinates of the bath
+    Output    :
+        The system Hamiltonian at R
+    Procedure :
+        H(R) → (ε₁ + δε₁)|1⟩⟨1| + (ε₂ + δε₂)|2⟩⟨2| + Δ(|1⟩⟨2| + |2⟩⟨1|)
+    """
+    
     Hij       = np.zeros((param.NStates, param.NStates), dtype=np.complex128)
     ε_sb      = H_sb(R)
     
@@ -28,6 +50,17 @@ def H_sys(R):
 
 @nb.jit(nopython=True)
 def F_nν(R, ψ):
+    """
+    Description:
+        Generates the system dependent force on R.
+    Input     : 
+        R  → current position coordinates of the bath
+        ψ  → current wavefunction of the system
+    Output    :
+        The Force on bath coordinates
+    Procedure :
+        Fᵤ⁽ⁱ⁾ = -ωᵤ⁽ⁱ⁾² Rᵤ⁽ⁱ⁾ + gᵤ⁽ⁱ⁾ Rᵤ⁽ⁱ⁾
+    """
     
     F    = -param.ω_nν_NM_sq * R
     ψ2   = np.absolute(ψ)**2
